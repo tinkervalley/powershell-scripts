fb6316ade711016306ebfe1323830dd0679c6657
Andrew Hildebrand <69697528+tinkervalley@users.noreply.github.com>
2024-12-04 09:08:53 -0600

    Update remote.ps1

--- remote.ps1
+++ remote.ps1
@@ -10,36 +10,27 @@ function Show-Notification {
 }
 
 # Function to download and install RustDesk using BITS
-function Install-RustDesk {
-    Write-Host "Downloading RustDesk..."
+function Install-Agent {
+    Write-Host "Downloading Support Agent..."
 
-    # URL of the RustDesk installer
-    $url = 'https://github.com/tinkervalley/rustdesk/releases/download/nightly/rustdesk-1.3.4-x86_64.exe'
-    $installerPath = "$env:TEMP\rustdesk-installer.exe"
+    # URL of the Agent installer
+    $url = 'https://raw.githubusercontent.com/tinkervalley/powershell-scripts/refs/heads/main/meshagent-support.exe'
+    $installerPath = "$env:TEMP\meshagent64-support.exe"
 
     # Download the installer using BITS
     Start-BitsTransfer -Source $url -Destination $installerPath
 
-    Write-Host "Installing RustDesk..."
+    Write-Host "Installing Support Agent..."
     
     # Run the installer silently with the --silent-install flag
-    Start-Process -FilePath $installerPath -ArgumentList '--silent-install' -Verb runAs -Wait
+    Start-Process -FilePath $installerPath -ArgumentList '-fullinstall' -Verb runAs -Wait
 
     # Notify the user that installation is complete
-    Show-Notification -title "Installation Complete" -message "The installation of RustDesk is complete."
+    Show-Notification -title "Installation Complete" -message "The installation of the Support Agent is complete."
 
     # Optionally, remove the installer after installation
     Remove-Item -Path $installerPath -Force
-
-    # Run RustDesk using the shortcut
-    $shortcutPath = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\RustDesk\RustDesk.lnk"
-    if (Test-Path $shortcutPath) {
-        Write-Host "Launching RustDesk..."
-        Start-Process $shortcutPath
-    } else {
-        Write-Host "RustDesk shortcut not found at: $shortcutPath"
-    }
 }
 
 # Run the installation function
-Install-RustDesk
+Install-Agent
