1d72e41d37762f3ff4cdb0d3f4693b9801304344
Andrew Hildebrand <69697528+tinkervalley@users.noreply.github.com>
2024-12-04 00:11:20 -0600

    Update check-elevation.ps1

--- check-elevation.ps1
+++ check-elevation.ps1
@@ -2,10 +2,10 @@
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     # Relaunch the script as Administrator
     $scriptPath = $MyInvocation.MyCommand.Path
-    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
+    Start-Process powershell -ArgumentList "-NoExit -NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
     Exit
 }
 
 # Script is elevated, now run the main script
-$mainScriptUrl = "https://raw.githubusercontent.com/tinkervalley/powershell-scripts/refs/heads/main/tools.ps1"
-Start-Process powershell -ArgumentList "-NoExit", "-Command", "& { irm $mainScriptUrl | iex }" -Verb RunAs
+$mainScriptPath = ".\tools.ps1"  # Adjust path as needed
+Start-Process powershell -ArgumentList "-NoExit -NoProfile -ExecutionPolicy Bypass -File `"$mainScriptPath`"" -Verb RunAs
