d31cb8b35362f2460173ab9f9cbe6462926de859
Andrew Hildebrand <69697528+tinkervalley@users.noreply.github.com>
2024-12-04 06:45:44 -0600

    Update tools-runner.ps1

--- tools-runner.ps1
+++ tools-runner.ps1
@@ -3,4 +3,4 @@ Write-Host "Loading main menu..." -BackgroundColor Blue -ForegroundColor White
 Start-Sleep -Seconds 2
 
 # Execute the remote script with 'runas' to ensure it's run as admin
-Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"irm https://raw.githubusercontent.com/tinkervalley/powershell-scripts/main/tools.ps1 | iex`"" -Verb RunAs
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"irm https://raw.githubusercontent.com/tinkervalley/powershell-scripts/main/tools/start.ps1 | iex`"" -Verb RunAs
