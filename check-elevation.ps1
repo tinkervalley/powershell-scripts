f718780cfe84eb5bb5e16d5cea7211df490fdfa6
Andrew Hildebrand <69697528+tinkervalley@users.noreply.github.com>
2024-12-04 00:05:07 -0600

    Update check-elevation.ps1

--- check-elevation.ps1
+++ check-elevation.ps1
@@ -3,8 +3,9 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     # Relaunch the script as Administrator
     $scriptPath = $MyInvocation.MyCommand.Path
     Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`"" -Verb RunAs
-    exit
+    Exit
 }
 
-# Your elevated script logic goes here
-Write-Host "Running with elevated permissions!"
+# Script is elevated, now run the main script
+$mainScriptUrl = "https://raw.githubusercontent.com/tinkervalley/powershell-scripts/refs/heads/main/tools.ps1"
+Start-Process powershell -ArgumentList "-NoExit", "-Command", "& { irm $mainScriptUrl | iex }" -Verb RunAs
