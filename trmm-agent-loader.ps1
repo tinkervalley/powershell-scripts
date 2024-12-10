7b8408a182ab8d494e950e86cdaf0a32005cb1eb
Andrew Hildebrand <69697528+tinkervalley@users.noreply.github.com>
2024-12-10 11:06:33 -0600

    Create trmm-agent-loader.ps1

---
+++ trmm-agent-loader.ps1
@@ -0,0 +1,11 @@
+# PowerShell script to load the main menu, close the current window, and run a remote script as admin in a new window
+
+# Display the loading message in the original window
+Write-Host "Loading installer..." -BackgroundColor Blue -ForegroundColor White
+Start-Sleep -Seconds 2
+
+
+# Execute the remote script in a new PowerShell window with 'runas' and customize window appearance
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"irm https://raw.githubusercontent.com/tinkervalley/powershell-scripts/main/trmm-agent.ps1 | iex`"" -Verb RunAs -WindowStyle Normal
+
+exit
