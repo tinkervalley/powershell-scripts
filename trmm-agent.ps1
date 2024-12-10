759d66af83f6d814584bd2f0b351608b2070da63
Andrew Hildebrand <69697528+tinkervalley@users.noreply.github.com>
2024-12-10 14:36:13 -0600

    Update trmm-agent.ps1

--- trmm-agent.ps1
+++ trmm-agent.ps1
@@ -1,4 +1,16 @@
-# Define the download URL and file path
+# Define the uninstaller path
+$uninstallerPath = "C:\Program Files\TacticalAgent\unins000.exe"
+
+# Check if the uninstaller exists
+if (Test-Path -Path $uninstallerPath) {
+    Write-Host "Uninstaller found. Running uninstaller..."
+    Start-Process -FilePath $uninstallerPath -ArgumentList "/VERYSILENT" -Wait
+    Write-Host "Uninstallation completed."
+} else {
+    Write-Host "No uninstaller found. Skipping uninstallation."
+}
+
+# Define the download URL
 $url = "https://github.com/amidaware/rmmagent/releases/download/v2.8.0/tacticalagent-v2.8.0-windows-amd64.exe"
 
 # Get the temporary directory and define the file name
@@ -8,23 +20,26 @@ $fileName = "trmm-agent-tinkervalley.exe"
 # Combine the temp directory with the file name to create the full download path
 $downloadPath = Join-Path -Path $tempDir -ChildPath $fileName
 
+# Output the download path for debugging
+# Write-Host "Download path: $downloadPath"
+
 # Use BITS to download the file
 Write-Host "Downloading the file..."
 Start-BitsTransfer -Source $url -Destination $downloadPath
 
-# Wait for the download to complete
+# Continue with the installation process
 Write-Host "Download complete. Installing TacticalAgent..."
-
-# Install the downloaded file with the given command
 Start-Process -FilePath $downloadPath -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES" -Wait
 
-# Wait 5 seconds before running the next command (simulating 'ping' delay)
+# Wait 10 seconds
 Start-Sleep -Seconds 10
 
-# Run the final command to install TacticalAgent using Start-Process
+# Define the final installation command and arguments
 $installCommand = "C:\Program Files\TacticalAgent\tacticalrmm.exe"
 $arguments = "-silent", "-m", "install", "--api", "https://api.tinkervalley.ca", "--client-id", "3", "--site-id", "3", "--agent-type", "workstation", "--auth", "5dc6b4395afc11b841b95aeb9b950e36d5622e2420af97a3171f37ffd5914d45"
 
+# Run the final installation command
+Write-Host "Running TacticalAgent installation command..."
 Start-Process -FilePath $installCommand -ArgumentList $arguments -Wait
 
 Write-Host "Installation completed."
