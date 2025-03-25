7fdb08ea612daa19f8e1f5263ed8f786ba0263b2
Andrew Hildebrand <69697528+tinkervalley@users.noreply.github.com>
2025-03-25 16:57:47 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "Hello There!"
+
+# Display loading message
+echo "Loading script..."
+
+# Define the URL of the script
+SCRIPT_URL="https://raw.githubusercontent.com/tinkervalley/powershell-scripts/refs/heads/main/tools-mac/start.sh"
+
+# Download and execute the script with sudo
+curl -fsSL "$SCRIPT_URL" | sudo bash
